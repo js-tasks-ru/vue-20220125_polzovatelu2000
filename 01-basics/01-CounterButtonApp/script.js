32ebdbdbee55f90d7dc64ba85cde8fcfd8d88f56
Vlad <polzovatelu2000@gmail.com>
2022-02-19 19:55:04 +0200

    Calculator

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,4 +1,4 @@
-import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 const vm = createApp({
     data() {
       return {
--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vm = createApp({
+    data(){
+    return {
+        number_first: 0,
+        number_second:0,
+        simbolOperation:""
+        };
+    },
+    computed:{
+        answer(){
+            switch (this.simbolOperation){
+                case "sum":
+                return this.number_first + this.number_second
+                case "subtract":
+                return this.number_first - this.number_second
+                case "multiply":
+                return this.number_first * this.number_second
+                case "divide":
+                return this.number_first / this.number_second
+                default: return 0;
+            }
+        }
+    }
+}).mount("#app");
+window.vm = vm;
+// const vm = createApp({
+//     data() {
+//       return {
+//         count: 0,
+//       };
+//     },
+//     methods:{
+//         counter(){
+//             this.count+=1
+//         }
+//     }
+//   }).mount('#app');

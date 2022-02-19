acb69e9263e1b9fee75e784a7ffef32a989bdb98
Vlad <polzovatelu2000@gmail.com>
2022-02-19 18:51:14 +0200

    Counter

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,14 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
-
-// Создайте Vue приложение
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+const vm = createApp({
+    data() {
+      return {
+        count: 0,
+      };
+    },
+    methods:{
+        counter(){
+            this.count+=1
+        }
+    }
+  }).mount('#app');
+window.vm = vm;
